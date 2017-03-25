709663ad16dba9c06f6ab070452fc35886ca399c
Chushu Liu <chushul512@gmail.com>
2017-03-25 01:41:19 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function() {
+	$("#cl").mouseenter(function spin() {
+		$("#cl").css("animation", "x 1s linear 0s infinite");
+    });
+});
+
+$(document).ready(function() {
+	$("#cl").mouseleave(function goback() {
+		$("#cl").css("animation", "x 15s linear 0s infinite");
+    });
+});
